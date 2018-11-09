5cc1ca2f8fe0b3d082783a9640bef63fd1491140
ShiehNg <shiehng@gmail.com>
2018-11-09 13:21:53 +0800

    添加了作者信息

---
+++ HUST_score_query/score_query.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+    :author: Wu Xie (吴谢)
+    :url: http://hellowuxie.com
+    :copyright: © 2018 Wu Xie <shiehng@gmail.com>
+    :license: MIT, see LICENSE for more details.
+"""
+
